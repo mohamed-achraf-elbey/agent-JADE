8f8058196ad3be3ce5ddc030d31cc2db7770a617
mohamed achraf <achrafelbey07@gmail.com>
2024-11-06 21:43:17 +0100

    change name class

---
+++ Agent2/src/pk/Agenttt.java
@@ -0,0 +1,10 @@
+package pk;
+
+import jade.core.Agent;
+
+public class Agenttt extends Agent {
+	   @Override
+	    protected void setup() {
+	        System.out.println("Agent " + getLocalName() + " is starting.");
+	    }
+}
